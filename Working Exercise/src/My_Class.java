c87c5d8c4f0a7fc67837b83becb38bb1d1bf25fd
makluv <b.sicobo@warwick.ac.uk>
2017-08-05 17:26:47 +0100

    Practicing Interfaces and Inner Classes

---
+++ Working Exercise/src/My_Class.java
@@ -0,0 +1,12 @@
+
+public class My_Class {
+
+	public static void main(String args[]) {
+	      // Instantiating the outer class 
+	      FirstInnerClass outer = new FirstInnerClass();
+	      
+	      // Accessing the display_Inner() method.
+	      outer.display_Inner();
+	   }
+	
+}
